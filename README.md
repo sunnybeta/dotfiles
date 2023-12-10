c557434bb8b615f49da79ee68182d8e5844a731b
Sunny <104438126+sunnybeta@users.noreply.github.com>
2023-12-10 14:16:33 +0530

    Feat: Removed Unnecessary Dependencies

--- README.md
+++ README.md
@@ -8,9 +8,3 @@ This repo consists of my dotfiles for Ubuntu 20.04.
 # Command to remove headerbar
 gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
 ```
-
-## Todo
-
-- [ ] Write additional setup and installation commands for ubuntu
-- [ ] i3wm + polybar setup on Void Linux
-- [ ] translate vim colors to neovim themes :O
--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.g.maplocalleader = '\\'
 vim.o.background = 'dark'
 vim.o.breakindent = true
 vim.o.clipboard = 'unnamedplus'
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menu,menuone,noselect'
 vim.o.expandtab = false
 vim.o.ignorecase = true
 vim.o.shiftwidth = 4
@@ -26,7 +26,7 @@ vim.opt.autoindent = true
 vim.opt.background = 'dark'
 vim.opt.backspace = 'start,eol,indent'
 vim.opt.breakindent = true
-vim.opt.cmdheight = 1
+vim.opt.cmdheight = 0
 vim.opt.cursorline = false
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
@@ -63,10 +63,10 @@ vim.keymap.set('i', '+-',   '±')
 vim.keymap.set('i', '<--',  '←')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('n', '[d',        vim.diagnostic.goto_prev,  { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d',        vim.diagnostic.goto_next,  { desc = 'Go to next diagnostic message'     })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message'  })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list'             })
+vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError', numhl = '' })
+vim.fn.sign_define('DiagnosticSignWarn',  { text = ' ', texthl = 'DiagnosticSignWarn',  numhl = '' })
+vim.fn.sign_define('DiagnosticSignHint',  { text = '󰠠 ', texthl = 'DiagnosticSignHint',  numhl = '' })
+vim.fn.sign_define('DiagnosticSignInfo',  { text = ' ', texthl = 'DiagnosticSignInfo',  numhl = '' })
 vim.cmd 'au BufWritePost */xresources !xrdb -load %'
 vim.cmd 'au BufWritePost */.bashrc !source ~/.bashrc'
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
@@ -81,11 +81,10 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup({
+require'lazy'.setup({
 	{
-		'sunnybeta/icecream',
+		'liminalminds/icecream.nvim',
 		name = 'icecream',
-		branch = 'neovim',
 		lazy = false,
 		priority = 1000,
 	},
@@ -117,69 +116,29 @@ require('lazy').setup({
 		lazy=false,
 		event = {'BufReadPre','BufNewFile'},
 		dependencies = {
-			'hrsh7th/nvim-cmp',
-			'L3MON4D3/LuaSnip',
-			'hrsh7th/cmp-nvim-lsp',
-			'williamboman/mason-lspconfig.nvim',
 			'williamboman/mason.nvim',
+			'hrsh7th/nvim-cmp',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
 		},
-	},
+	}
 })
 vim.cmd.colorscheme 'icecream'
-vim.api.nvim_set_hl(0, 'Normal',      { bg = 'None' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'None' })
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'None' })
-local colors = {
-	black      = '#111111',
-	lightgray  = '#9387c0',
-	orange     = '#206bc4',
-	purple     = '#583c6f',
-	red        = '#f00359',
-	yellow     = '#fdba21',
-	green      = '#23855d',
-	white      = '#f00359',
-}
-local lualine_theme = {
-	normal = {
-		a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-	insert = {
-		a = { bg = colors.green, fg = colors.black, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-	visual = {
-		a = { bg = colors.purple, fg = colors.black, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-	replace = {
-		a = { bg = colors.red, fg = colors.black, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-	command = {
-		a = { bg = colors.orange, fg = colors.black, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-	inactive = {
-		a = { bg = colors.black, fg = colors.white, gui = 'bold' },
-		b = { bg = colors.lightblack, fg = colors.white },
-		c = { bg = colors.black, fg = colors.white },
-	},
-}
-require('lualine').setup({
+-- vim.api.nvim_set_hl(0, 'Normal',      { bg = 'None' })
+-- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'None' })
+-- vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'None' })
+require'lualine'.setup({
 	options = {
 		icons_enabled = true,
-		theme = lualine_theme,
+		theme = 'auto',
 		component_separators = { left = '┊', right = '┊'},
 		section_separators   = { left = ' ' , right = ' '},
 	}
 })
-require('telescope').setup({
+require'telescope'.setup({
 	defaults = {
 		mappings = {
 		  i = {
@@ -189,13 +148,14 @@ require('telescope').setup({
 		},
 	},
 })
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files,   { desc = '[S]earch Gi[T] Files'    })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,  { desc = '[S]earch [F]iles'        })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,   { desc = '[S]earch [H]elp'         })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,   { desc = '[S]earch by [G]rep'      })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics'  })
-require('nvim-treesitter.configs').setup({
+vim.keymap.set('n', '<leader>gf', require'telescope.builtin'.git_files,   { desc = '[S]earch Gi[T] Files'    })
+vim.keymap.set('n', '<leader>sf', require'telescope.builtin'.find_files,  { desc = '[S]earch [F]iles'        })
+vim.keymap.set('n', '<leader>sh', require'telescope.builtin'.help_tags,   { desc = '[S]earch [H]elp'         })
+vim.keymap.set('n', '<leader>sw', require'telescope.builtin'.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require'telescope.builtin'.live_grep,   { desc = '[S]earch by [G]rep'      })
+vim.keymap.set('n', '<leader>sd', require'telescope.builtin'.diagnostics, { desc = '[S]earch [D]iagnostics'  })
+
+require'nvim-treesitter.configs'.setup({
 	ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'typescript', 'python', 'markdown', 'bash', 'go', 'rust' },
 	ignore_install = {},
 	modules = {},
@@ -209,68 +169,56 @@ require('nvim-treesitter.configs').setup({
 		enable = true,
 	}
 })
-local cmp = require('cmp')
-cmp.setup({
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({
-		  behavior = cmp.ConfirmBehavior.Replace,
-		  select = true,
-		}),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-	}),
-})
-local on_attach = function(_, bufnr)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'LSP: [G]oto [D]efinition'})
-end
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('mason').setup({
+require'mason'.setup({
 	ui={
 		icons={
 			package_installed   = '✓',
 			package_pending     = '󰠠',
 			package_uninstalled = '',
 		},
-		{
-			border = 'rounded',
-		}
 	}
 })
-require('mason-lspconfig').setup({
-	ensure_installed = {
-		'clangd',
-		'cssls',
-		'gopls',
-		'html',
-		'jdtls',
-		'lua_ls',
-		'marksman',
-		'pyright',
-		'rust_analyzer',
-		'tailwindcss',
-		'tsserver',
-		'volar'
+local cmp = require'cmp'
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require'luasnip'.lsp_expand(args.body)
+		end,
 	},
-	automatic_installation=true,
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	})
 })
-vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError', numhl = '' })
-vim.fn.sign_define('DiagnosticSignWarn',  { text = ' ', texthl = 'DiagnosticSignWarn',  numhl = '' })
-vim.fn.sign_define('DiagnosticSignHint',  { text = '󰠠 ', texthl = 'DiagnosticSignHint',  numhl = '' })
-vim.fn.sign_define('DiagnosticSignInfo',  { text = ' ', texthl = 'DiagnosticSignInfo',  numhl = '' })
-lspconfig.lua_ls.setup({
-	capabilities = capabilities,
+local capabilities = require'cmp_nvim_lsp'.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local on_attach = function()
+	vim.keymap.set('n', 'gd',        vim.lsp.buf.   definition,      { buffer=0, desc='LSP: [G]oto [D]efinition'          })
+	vim.keymap.set('n', 'K',         vim.lsp.buf.   hover,           { buffer=0, desc='LSP: Hover [I]nformation'          })
+	vim.keymap.set('n', 'gt',        vim.lsp.buf.   type_definition, { buffer=0, desc='LSP: [G]oto [T]ype [D]efinition'   })
+	vim.keymap.set('n', 'gi',        vim.lsp.buf.   implementation,  { buffer=0, desc='LSP: [G]oto [I]mplementation'      })
+	vim.keymap.set('n', '<leader>r', vim.lsp.buf.   rename,          { buffer=0, desc='LSP: Rename Definition'            })
+	vim.keymap.set('n', '<leader>c', vim.lsp.buf.   code_action,     { buffer=0, desc='LSP: [C]ode Action'                })
+	vim.keymap.set('n', '[d',        vim.diagnostic.goto_prev,       { buffer=0, desc='Go To Previous Diagnostic Message' })
+	vim.keymap.set('n', ']d',        vim.diagnostic.goto_next,       { buffer=0, desc='Go To Next Diagnostic Message'     })
+	vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float,      { buffer=0, desc='Open Floating Diagnostic Message'  })
+	vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,      { buffer=0, desc='Open Diagnostics List'             })
+end
+require'lspconfig'.lua_ls.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 	settings = {
 		Lua = {
 			diagnositcs = {
@@ -285,47 +233,47 @@ lspconfig.lua_ls.setup({
 		}
 	}
 })
-lspconfig.pyright.setup({
-	capabilities = capabilities,
+require'lspconfig'.pyright.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.cssls.setup({
-	capabilities = capabilities,
+require'lspconfig'.cssls.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.gopls.setup({
-	capabilities = capabilities,
+require'lspconfig'.gopls.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.rust_analyzer.setup({
-	capabilities = capabilities,
+require'lspconfig'.rust_analyzer.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.jdtls.setup({
-	capabilities = capabilities,
+require'lspconfig'.jdtls.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.bashls.setup({
-	capabilities = capabilities,
+require'lspconfig'.bashls.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.html.setup({
-	capabilities = capabilities,
+require'lspconfig'.html.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.marksman.setup({
-	capabilities = capabilities,
+require'lspconfig'.marksman.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.tsserver.setup({
-	capabilities = capabilities,
+require'lspconfig'.tsserver.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.tailwindcss.setup({
-	capabilities = capabilities,
+require'lspconfig'.tailwindcss.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
-lspconfig.volar.setup({
-	capabilities = capabilities,
+require'lspconfig'.volar.setup({
 	on_attach = on_attach,
+	capabilities = capabilities,
 })
